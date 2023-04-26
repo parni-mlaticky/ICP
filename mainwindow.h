6cd26e8c7d00147f1cc2b0d147b95ed563d1cefd
KolouchPetr <kolouch.petr4@email.cz>
2023-04-26 10:17:03 +0200

    Updated mainwindow to work with level class

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "src/player.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QMainWindow>
 
 class MainWindow : public QMainWindow {
@@ -8,4 +11,9 @@ class MainWindow : public QMainWindow {
 public:
   explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow() override;
+
+private:
+  QGraphicsScene *m_scene;
+  QGraphicsView *m_view;
+  Player *m_player;
 };

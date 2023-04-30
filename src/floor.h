1b5a9c240836e0d3a5c66168848d0683cdcd2479
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2023-04-30 15:17:43 +0200

    Upgraded level

---
+++ src/floor.h
@@ -0,0 +1,10 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+#include "entity.h"
+
+class Floor : public Entity {
+public:
+  Floor(qreal x, qreal y, Level* level);
+
+};
+#endif
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,13 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), m_scene(new QGraphicsScene(this)),
       m_view(new QGraphicsView(m_scene, this)) {
 
+  const int height = 600;
+  const int width = 800;
+
   setWindowTitle("Parni pacman");
-  setFixedSize(800, 600);
+  setFixedSize(width, height);
   setCentralWidget(m_view);
   m_view->setRenderHint(QPainter::Antialiasing);
   m_view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
 
-  m_level = new Level(m_scene, 32);
+  m_level = new Level(m_scene, width, height);
   m_scene->setSceneRect(-100, -100, 200, 200);
   m_level->loadLevel("./levels/level.txt");
 

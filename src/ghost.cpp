775d324d5390d41f62e850349fb3ed1df253b416
Ondřej Zobal <ondrej@ondrejzobal.xyz>
2023-05-02 22:14:21 +0200

    Implemented R* for ghosts

--- src/ghost.cpp
+++ src/ghost.cpp
@@ -1,5 +1,41 @@
 #include "ghost.h"
 #include <QKeyEvent>
 #include <QPainter>
+#include <iostream>
 
-Ghost::Ghost(int x, int y, DrawableItem* item) : Entity(x, y, item, 'G') {m_can_move = true;}
+Ghost::Ghost(int x, int y, DrawableItem* item) : Entity(x, y, item, 'G'), m_steps_left(1) {
+    m_can_move = true;
+    m_allowed_directions = std::vector<std::pair<int, int>>();
+}
+
+void Ghost::update() {
+    std::cerr << "brih" << std::endl;
+    --m_steps_left;
+
+    bool colided = true;
+    for (auto dir : m_allowed_directions) {
+        if(dir.first == m_dx && dir.second == m_dy) {
+            colided = false;
+            break;
+        }
+    }
+
+    if (!m_steps_left || colided) {
+        newDirection();
+    }
+}
+
+void Ghost::newDirection() {
+    if (m_allowed_directions.size() == 0) {return;}
+    int r = rand() % m_allowed_directions.size();
+    m_steps_left = (rand() % 5);
+
+    auto direction = m_allowed_directions[r];
+
+    m_dx = direction.first;
+    m_dy = direction.second;
+}
+
+void Ghost::setAllowedDirections(std::vector<std::pair<int, int>> directions) {
+    m_allowed_directions = directions;
+}
--- src/ghost.h
+++ src/ghost.h
@@ -7,5 +7,12 @@
 class Ghost : public Entity {
 public:
   Ghost(int x, int y, DrawableItem* item);
+  void update() override;
+  void setAllowedDirections(std::vector<std::pair<int, int>> directions);
+private:
+  void newDirection();
+  int m_steps_left;
+  std::vector<std::pair<int, int>> m_allowed_directions;
+
 };
 #endif

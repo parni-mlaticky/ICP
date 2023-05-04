6c28a80af7fe76c73c43d28727e8e0c2ecf6d777
KolouchPetr <kolouch.petr4@email.cz>
2023-05-04 10:51:54 +0200

    Level is now loading from std::string, added function for file reading

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,13 +1,14 @@
 #include "mainwindow.h"
 #include "client.h"
+#include "scene.h"
 #include "server.h"
 #include <QDebug>
+#include <QKeyEvent>
 #include <QTimer>
 #include <QVBoxLayout>
 #include <chrono>
+#include <iostream>
 #include <thread>
-#include <QKeyEvent>
-#include "scene.h"
 
 MainWindow::MainWindow(QString &levelFilePath, QWidget *parent)
     : QMainWindow(parent), m_levelLoaded(false) {
@@ -16,21 +17,23 @@ MainWindow::MainWindow(QString &levelFilePath, QWidget *parent)
   const int width = 800;
 
   this->m_log_tick_ms = 200;
-  this->m_gfx_tick_ms = 1000/60;
+  this->m_gfx_tick_ms = 1000 / 60;
 
   this->m_animation_frames = m_log_tick_ms / m_gfx_tick_ms;
-  this->m_frame_counter = m_animation_frames-1;
-  this->m_scene = new Scene(width, height, this->m_animation_frames, (MainWindow*) this);
+  this->m_frame_counter = m_animation_frames - 1;
+  this->m_scene =
+      new Scene(width, height, this->m_animation_frames, (MainWindow *)this);
 
-  this->m_view = new QGraphicsView((Scene*) m_scene, this);
+  this->m_view = new QGraphicsView((Scene *)m_scene, this);
   setWindowTitle("Parni pacman");
   setFixedSize(width, height);
   setCentralWidget(m_view);
   m_view->setRenderHint(QPainter::Antialiasing);
   m_view->setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
 
-  m_level = new Level((Drawable*) m_scene);
-  m_level->loadLevel(levelFilePath);
+  m_level = new Level((Drawable *)m_scene);
+  std::string levelString = this->loadLevelFile(levelFilePath);
+  m_level->loadLevel(levelString);
 
   QTimer *timer = new QTimer(this);
 
@@ -38,7 +41,20 @@ MainWindow::MainWindow(QString &levelFilePath, QWidget *parent)
   timer->start(this->m_gfx_tick_ms);
 }
 
-void MainWindow::keyPressEvent(QKeyEvent* event) {
+std::string MainWindow::loadLevelFile(QString levelFilePath) {
+  QFile file(levelFilePath);
+  if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    std::cerr << "Failed to open the file containing level" << std::endl;
+    exit(1);
+  }
+  QTextStream in(&file);
+  QString level = in.readAll();
+  file.close();
+
+  return level.toStdString();
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event) {
   this->m_level->keyPressEvent(event);
 }
 
